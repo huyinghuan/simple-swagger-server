93119f8d94229dbc9976a4dfdbbc1e5c6b2a33f5
yinghuan <ec.huyinghuan@gmail.com>
2023-08-19 01:58:04 +0800

    添加上传配置功能

--- main.go
+++ main.go
@@ -61,7 +61,20 @@ func NewApp(docsDir string) *gin.Engine {
 		ctx.JSON(200, files)
 	})
 	defaultDocsDir := docsDir // 默认文档目录 ./docs/xxx docs/xxx
-
+	router.POST("/api/upload", func(ctx *gin.Context) {
+		file, err := ctx.FormFile("file")
+		if err != nil {
+			ctx.JSON(501, gin.H{"error": err.Error()})
+			return
+		}
+		fileName := file.Filename
+		filePath := path.Join(defaultDocsDir, fileName)
+		if err := ctx.SaveUploadedFile(file, filePath); err != nil {
+			ctx.JSON(501, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(200, gin.H{"message": "上传成功"})
+	})
 	router.GET("/*filepath", func(ctx *gin.Context) {
 		file := ctx.Param("filepath")
 		file = strings.Replace(file, "..", "", -1)
